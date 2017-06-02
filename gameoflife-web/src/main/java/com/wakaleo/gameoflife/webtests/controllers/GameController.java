eea5fd25d494ae5b7a287d6dd1109d322e2082c9
Bambang Hermanto <bambang@erpz.com>
2017-06-02 11:46:13 -0700

    testing

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -16,7 +16,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  * Game of life, demonstration application for Jenkins: The Definitive Guide
  *
  * GameController.java
- * Controller to navigate the buttons on the game pages
+ * Controller to navigate the buttons on the. game pages
  */
 @Controller
 @RequestMapping("/game")
